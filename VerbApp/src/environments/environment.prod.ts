b722d22cf691e81a684e922218e01e353d0c7a64
delaney <delaney.lothian@gmail.com>
2026-01-22 10:47:42 -0700

    environment and fixed workflow

--- VerbApp/src/environments/environment.prod.ts
+++ VerbApp/src/environments/environment.prod.ts
@@ -1,11 +1,23 @@
+import { EveryVoiceConfig } from "@everyvoice/every-voice";
+
+export const everyVoiceConfig: EveryVoiceConfig = {
+
+  enableTTS: true,
+  requiresAuth: true,
+  apiUrl: "https://unbq-sgile-text-to-speech.hf.space",
+  availableSpeakers: [{ display: "Tina", slug: "tina" }, {display: "Wayne", slug: "wayne"}], // Optional: Available Speaker IDs for TTS
+  defaultSpeaker: { display: "Tina", slug: "tina" }, // Optional: Speaker ID for TTS
+  diffusionSteps: 3, // Optional: Number of steps for TTS
+  // domain: "unbq-everyvoice.ca.auth0.com", // Optional: Auth0 Domain
+  domain: "dev-qioor1nubb5vu3uw.ca.auth0.com",
+  clientId: "7jLB2de2XqEGsAbXLEnUjB49P35r8gQH", // Optional: Auth0 ClientId
+  // middlewareEndpoint: "http://localhost:3000/tts", // Optional: Middleware endpoint
+  audience: "https://unbq.everyvoice.middleware.ca", // Optional: Auth0 audience
+  // audience: "test-middleware",
+}
+
+
 export const environment = {
   production: true,
-  ttsConfig: {
-    apiUrl: "https://unbq-sgile-text-to-speech.hf.space/gradio_api/queue/", // 👈 your TTS backend endpoint
-    enableTTS: true, // 👈 set this to false if you want to disable your TTS in certain deployment environments. Note, if this is disabled, your EveryVoice components will not render and TTS will not be accessible from your application.
-    developmentBearerToken: "", // 👈 OPTIONAL authentication token if required by your API. It will be treated as an Authorization Bearer token
-    speakerID: "tina", // 👈 OPTIONAL speaker id.
-    requiresAuth: false,
-    diffusionSteps: 3, // 👈  OPTIONAL: number of diffusion steps
-  },
+  ttsConfig: everyVoiceConfig,
 };
