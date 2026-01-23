2c950dcd42f2e581f27015a7967fe1bcaa45fb1a
delaney <delaney.lothian@gmail.com>
2026-01-23 09:38:41 -0700

    reset env variables for testing

--- VerbApp/src/environments/environment.prod.ts
+++ VerbApp/src/environments/environment.prod.ts
@@ -8,13 +8,13 @@ export const everyVoiceConfig: EveryVoiceConfig = {
   availableSpeakers: [{ display: "Tina", slug: "tina" }, {display: "Wayne", slug: "wayne"}], // Optional: Available Speaker IDs for TTS
   defaultSpeaker: { display: "Tina", slug: "tina" }, // Optional: Speaker ID for TTS
   diffusionSteps: 3, // Optional: Number of steps for TTS
-  domain: "unbq-everyvoice.ca.auth0.com", // Optional: Auth0 Domain
-  clientId: "7jLB2de2XqEGsAbXLEnUjB49P35r8gQH", // Optional: Auth0 ClientId
+  // domain: "unbq-everyvoice.ca.auth0.com", // Optional: Auth0 Domain
+  // clientId: "7jLB2de2XqEGsAbXLEnUjB49P35r8gQH", // Optional: Auth0 ClientId
   middlewareEndpoint: "https://unbq-crk-tts-middleware-ee6d73382adb.herokuapp.com/tts", // Optional: Middleware endpoint
-  audience: "https://unbq.everyvoice.middleware.ca", // Optional: Auth0 audience
-  // domain: "dev-qioor1nubb5vu3uw.ca.auth0.com", // Optional: Auth0 Domain
-  // clientId: "6jhqRWJ6YM9zKsmwzq55y64zv4N13Jfe", // Optional: Auth0 ClientId
-  // audience: "test-middleware"
+  // audience: "https://unbq.everyvoice.middleware.ca", // Optional: Auth0 audience
+  domain: "dev-qioor1nubb5vu3uw.ca.auth0.com", // Optional: Auth0 Domain
+  clientId: "6jhqRWJ6YM9zKsmwzq55y64zv4N13Jfe", // Optional: Auth0 ClientId
+  audience: "test-middleware"
 }
 
 
